24a6848d6d16dcd9a3c5675c4f95c220b58deac5
PaipaPsyche <paipamurderer@gmail.com>
2019-02-10 00:48:56 -0500

    implementando 6 conectividad

---
+++ div_test/grid_div.py
@@ -0,0 +1,143 @@
+"""
+Created on Sat Feb  9 22:58:03 2019
+
+@author: david
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from time import gmtime, strftime
+
+
+DATA=np.load("div_test.npy")
+
+n_side=DATA.shape[0]
+
+#=========================FUNCIONES=======================
+def vecinos3D(ii,jj,kk,tam_r):
+    ii_a=(ii-1)%tam_r
+    ii_b=(ii+1)%tam_r
+    jj_a=(jj-1)%tam_r
+    jj_b=(jj+1)%tam_r
+    kk_a=(kk-1)%tam_r
+    kk_b=(kk+1)%tam_r 
+    return [ii_a,ii_b,ii,ii,ii,ii],[jj,jj,jj_a,jj_b,jj,jj],[kk,kk,kk,kk,kk_a,kk_b]
+
+def revisarVecinos(ii,jj,kk,Pertenencia,tam_r):
+    xx,yy,zz=vecinos3D(ii,jj,kk,tam_r)
+    tam=len(xx)
+    pert=[]
+    for i in range (tam):
+        p_i=Pertenencia[xx[i],yy[i],zz[i]]
+        pert.append(p_i)
+    suma=np.sum(pert)
+    if(suma==0):
+        return 0
+    else:
+        pert=np.array(pert)
+        return int(np.random.choice(pert[pert!=0]))
+        
+
+
+def dist_nodo_cercano(ii,jj,kk,x,y,z):
+    d=np.inf
+    xx=0
+    yy=0
+    zz=0
+    for i in range (len(x)):
+        dist=np.sqrt((x[i]-ii)**2+(y[i]-jj)**2+(z[i]-kk)**2)
+        if (dist<d):
+            d=dist
+            xx=x[i]
+            yy=y[i]
+            zz=z[i]
+    return d,xx,yy,zz
+
+
+def darVacios(Pertenencia):
+    return len(np.where(Pertenencia==0)[0])
+
+#===================WATERSHED==============================
+
+def watershed(filename,filename_out,d_tolerancia,vacios_tolerancia):
+    DIV=np.load(filename)
+    DIV=DIV[1:-1,1:-1,1:-1]
+    tam=np.shape(DIV)[0]
+    N_intervalos=20*tam
+    
+    div_max=round(np.amax(DIV)+5,-1)
+    div_min=round(np.amin(DIV)-5,-1)
+    N_intervalos=7*tam
+    
+    
+    barridoA=np.linspace(div_max,div_min,N_intervalos)
+    barrido=np.concatenate((barridoA,barridoA))
+    
+    tam_b=len(barrido)
+    delta=barrido[0]-barrido[1]
+    Pertenencia=np.zeros([tam,tam,tam])
+    
+    
+    x_nodos=[]
+    y_nodos=[]
+    z_nodos=[]
+    cont_nodos=1
+    
+    cont_iteraciones=0
+    
+    
+    vacios=[]
+    vacios.append(darVacios(Pertenencia))
+    while(vacios[-1]>vacios_tolerancia):
+        print(str(round((100*vacios[-1]/(tam**3)),6)) + " % del espacio está vacio ("+str(vacios[-1])+" voxeles)")   
+        c=0
+        for lev in barrido:
+            if(c%200==0):
+                print(str(round(c*100/tam_b,2))+" % evaluado ")
+            c+=1
+            cota_inf=lev
+            cota_sup=lev+delta
+            aa=np.where((DIV<cota_sup)&(DIV>=cota_inf)&(Pertenencia==0))
+            xx,yy,zz=aa[0],aa[1],aa[2]
+            rec=len(xx)
+            for i in range (rec):
+                x,y,z=xx[i],yy[i],zz[i]
+                pert=revisarVecinos(x,y,z,Pertenencia,tam)
+                if(pert!=0):
+                    Pertenencia[x,y,z]=pert
+                else:
+                    d,ii,jj,kk=dist_nodo_cercano(x,y,z,x_nodos,y_nodos,z_nodos)
+                    if(d>d_tolerancia):
+                        Pertenencia[x,y,z]=cont_nodos
+                        cont_nodos+=1
+                        x_nodos.append(x)
+                        y_nodos.append(y)
+                        z_nodos.append(z)
+        cont_iteraciones+=1
+        vacios.append(darVacios(Pertenencia))
+    aa=np.where(Pertenencia==0)
+    xx,yy,zz=aa[0],aa[1],aa[2]
+    rec=len(xx)
+    for i in range (rec):
+        d,ii,jj,kk=dist_nodo_cercano(xx[i],yy[i],zz[i],x_nodos,y_nodos,z_nodos)
+        Pertenencia[xx[i],yy[i],zz[i]]=Pertenencia[ii,jj,kk]
+
+    np.save(filename_out,Pertenencia)
+    return cont_nodos-1,cont_iteraciones
+
+
+
+#============Main======================================================
+    
+file_stats=open("STATS.csv","w")
+file_stats.write("Parches,Iteraciones,VaciosTol,DistanciaTol,FileIN,FileOut,Time"+"\n")
+
+name_in="div_test.npy"
+name_out="pert_test.npy"
+
+tol_vacios=0
+DistanciaTol=1
+
+nodos,it=watershed(name_in,name_out,DistanciaTol,tol_vacios)
+
+file_stats.write(str(nodos)+","+str(it)+","+str(tol_vacios)+","+str(DistanciaTol)+","+name_in+","+name_out+","+strftime("%Y.%m.%d-%H:%M:%S", gmtime())+"\n")
+file_stats.close()
---
+++ div_test/plot_pert.py
@@ -0,0 +1,43 @@
+"""
+Created on Sat Feb  9 23:28:10 2019
+
+@author: david
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+PERT=np.load("pert_test.npy")
+DIV=np.load("div_test.npy")[1:-1,1:-1,1:-1]
+
+n_side=DIV.shape[0]
+
+min_div=np.amin(DIV)
+max_div=np.amax(DIV)
+min_node=np.amin(PERT)
+max_node=np.amax(PERT)
+
+
+for i in range(n_side):
+    print(i)
+  
+    
+    fig = plt.figure(figsize=(20, 10))
+    fig.suptitle("Z = "+str(i)+"/"+str(n_side),fontsize=25)
+    ax1 = fig.add_subplot(121)
+    im1 = ax1.imshow((DIV[:,:,i]),cmap="nipy_spectral",vmax=max_div,vmin=min_div)
+    
+    divider = make_axes_locatable(ax1)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    fig.colorbar(im1, cax=cax, orientation='vertical')
+    
+    ax2 = fig.add_subplot(122)
+    im2 = ax2.imshow(PERT[:,:,i],cmap="gist_ncar",vmax=max_node,vmin=min_node)
+    
+    divider = make_axes_locatable(ax2)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    fig.colorbar(im2, cax=cax, orientation='vertical')
+    
+    fig.savefig("zsweep_"+str(i)+".png")
+    plt.close()
